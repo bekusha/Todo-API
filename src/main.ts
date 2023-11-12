86bd51abe6c56fa334d3c044c55535653f58c078
Jorjikia Beka <jorjikiabeka@Jorjikias-MacBook-Air.local>
2023-11-12 22:24:36 +0400

    cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions: CorsOptions = {
-    origin: 'http://localhost:4200', 
+    origin: 'https://funny-fox-23d964.netlify.app/', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: false,
   };
