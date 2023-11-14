18d489d828ba4ced79bc8f549058ba4ce1626dc0
Jorjikia Beka <jorjikiabeka@Jorjikias-MacBook-Air.local>
2023-11-14 12:04:47 +0400

    fix slash

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions: CorsOptions = {
-    origin: 'https://preeminent-melomakarona-678752.netlify.app/', 
+    origin: 'https://preeminent-melomakarona-678752.netlify.app', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   };
